20236a96a5e5ecdabda0cc91a458d013a372b3ea
Jimmy Fidiles <jfidiles@gmail.com>
2016-02-29 22:00:36 +0000

    Added pages

--- assets/js/util.js
+++ assets/js/util.js
@@ -583,5 +583,12 @@
 			});
 
 	};
+    //select the POPUP FRAME and show it
+       $("#popup").hide().fadeIn(1000);
 
+       //close the POPUP if the button with id="close" is clicked
+       $("#close").on("click", function (e) {
+           e.preventDefault();
+           $("#popup").fadeOut(1000);
+       });
 })(jQuery);
